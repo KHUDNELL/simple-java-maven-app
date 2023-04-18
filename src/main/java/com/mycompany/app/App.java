753663e47a1d4414ef2bac0a249d814a8a043bc3
Ken Hudnell <69016264+KHUDNELL@users.noreply.github.com>
2023-04-18 09:57:13 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! Todai is a great day!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
